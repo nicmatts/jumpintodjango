72e63b4fa16dcf9cdf5bed7523df3ac8193236c9
Nicole M <nicmatts@gmail.com>
2014-04-12 20:20:39 -0500

    moved file to correct folder

--- jumpintodjango/templates/questionsandanswers/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from questionsandanswers.models import Question, Answer
-# Register your models here.
-class QuestionAdmin(admin.ModelAdmin):
-	list_display = ('subject', 'description', 'publication_date',)
-
-admin.site.register(Question, QuestionAdmin)
